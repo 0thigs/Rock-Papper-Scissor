1db55f54f34c6ee81e7fb1862b41a8b1f8793663
0thigs <machinimavlogs@gmail.com>
2024-02-05 17:49:09 -0300

    Fix: Button Design Fixed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,20 @@
-import Image from "next/image"
-import Circle from "@/components/circle"
-import Logo from '../../public/images/logo.svg'
-import Triangle from '../../public/images/bg-triangle.svg'
-import Papper from '../../public/images/icon-paper.svg'
-import Scissor from '../../public/images/icon-scissors.svg'
-import Rock from '../../public/images/icon-rock.svg'
+import Image from "next/image";
+import Circle from "@/components/circle";
+import Logo from "../../public/images/logo.svg";
+import Triangle from "../../public/images/bg-triangle.svg";
+import Papper from "../../public/images/icon-paper.svg";
+import Scissor from "../../public/images/icon-scissors.svg";
+import Rock from "../../public/images/icon-rock.svg";
 
 export default function Home() {
+
+  const size = 110
+
   return (
     <section className="flex flex-col items-center justify-center gap-56">
       <header className="flex justify-between p-3 border-2 border-gray-600 rounded-lg w-96 lg:w-[500px]">
         <div className="flex flex-col items-center justify-center ">
-          <Image 
+          <Image
             src={Logo}
             width={80}
             alt="Pedra Papel ou Tesoura"
@@ -23,34 +26,40 @@ export default function Home() {
             SCORE
           </h1>
           <p className="text-5xl font-extrabold tracking-[-5px] font-barlow text-gray-700">
-          0
+            0
           </p>
         </div>
       </header>
 
       <main className="relative border-2">
+        <Image src={Triangle} width={200} alt="Pedra Papel ou Tesoura" />
 
-        <Image 
-            src={Triangle}
-            width={200}
-            alt="Pedra Papel ou Tesoura"
-            className="lg:w-24"
-          />
-          
-        
-
-          <Circle 
+        <Circle
           img={Papper}
-          size={"100"}
-          color={"red"}
+          size={size}
+          color={"#4A67F5"}
+          top={"-25%"}
+          left={"-25%"}
+        />
 
-          top={"-10px"}
-          right={"-30%"}
-          />
+        <Circle
+          img={Scissor}
+          size={size}
+          color={"#ECA822"}
+          top={"-25%"}
+          left={"75%"}
+        />
+
+        <Circle
+          img={Rock}
+          size={size}
+          color={"#DA425F"}
+          top={"75%"}
+          left={"25%"}
+        />
       </main>
     </section>
-  )
+  );
 }
-
 
 //BUG: CONSERTAR ERRO DO CIRCLE: PROPS NAO FUNCIONANDO CORRETAMENTE!
--- src/components/circle.jsx
+++ src/components/circle.jsx
@@ -1,28 +1,26 @@
 import Image from "next/image";
 
-export default function Circle({ img, size, top, right, color }) {
- // Definindo o estilo inline
+export default function Circle({ img, size, top, left, color }) {
  const circleStyle = {
     position: "absolute",
-    top: `${top}`,
-    right: `${right}`,
+    top: top, 
+    left: left,
     border: `13px solid ${color}`,
     width: `${size}px`,
     height: `${size}px`,
-    background: "linear-gradient(to top, slate-100, slate-400)",
+    background: "linear-gradient(to top, #F2F1F2, #DCDCDC)",
     borderRadius: "50%",
-    backgroundColor: "gray",
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
     boxShadow: "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
-    inset: "0"
  };
 
  return (
     <button style={circleStyle}>
       <Image 
+        width={35}
         src={img}
       />
     </button>

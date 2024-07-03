52dddca606177543e28db2bd9a9cc28b1807fdcc
0thigs <machinimavlogs@gmail.com>
2024-07-03 19:51:08 -0300

    🧱component: create rules

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer>
+            <button className="py-1 font-light text-white uppercase transition-all border border-white rounded-lg hover:scale-105 px-7">
+                Rules
+            </button>
+        </footer>
+    )
+}

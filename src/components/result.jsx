03868a71ecb7866464f39faa6ae7470f8fed24cb
0thigs <machinimavlogs@gmail.com>
2024-07-27 13:17:55 -0300

    🧱component: update result component

--- src/components/result.jsx
+++ src/components/result.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 function Result() {
     return (
-        <div>
-            <h1>Popup RESULT</h1>
-        </div>
+        <>
+            <div className="fixed inset-0 flex items-center justify-center transition-opacity duration-500 bg-black bg-opacity-50"></div>
+            <div className={`relative p-8 text-black bg-white rounded-lg animate-fade-inw-96`}>
+
+
+            </div>
+        </>
     );
 }
 

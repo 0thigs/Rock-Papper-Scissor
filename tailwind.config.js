9210731bd5bcc9a232c28eca8b27b61488577583
0thigs <machinimavlogs@gmail.com>
2024-07-26 21:50:06 -0300

    ⚙config: tailwind configurator

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,20 @@ module.exports = {
 	],
 	theme: {
 		extend: {
+			keyframes: {
+				'fade-in': {
+				  '0%': { opacity: 0, transform: 'scale(0.95)' },
+				  '100%': { opacity: 1, transform: 'scale(1)' },
+				},
+				'fade-out': {
+         			'0%': { opacity: 1, transform: 'scale(1)' },
+          			'100%': { opacity: 0, transform: 'scale(0.95)' },
+        	},
+			},
+			animation: {
+				'fade-in': 'fade-in 0.5s ease-out',
+				'fade-out': 'fade-out 0.5s ease-out',
+			},
 			backgroundImage: {
 				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
 				"gradient-conic":

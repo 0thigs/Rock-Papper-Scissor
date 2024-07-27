08f4407739497e58b6ca4c572d63a05a3383dda3
0thigs <machinimavlogs@gmail.com>
2024-07-26 21:51:12 -0300

    🖥script: add footer logic

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Rules from './rules';
 
 export default function Footer() {
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
         <footer>
-            <button className="py-1 font-light text-white uppercase transition-all border border-white rounded-lg hover:scale-105 px-7">
+            <button onClick={() => setIsOpen(!isOpen)} className="py-1 font-light text-white uppercase transition-all border border-white rounded-lg hover:scale-105 px-7">
                 Rules
             </button>
+            <Rules isOpen={isOpen} setIsOpen={setIsOpen} />
         </footer>
     )
 }
